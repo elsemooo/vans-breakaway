3ad6198e038117dfe5db2c761d21f7a2e7958b65
khalid samanamud <khalidsamanamud@gmail.com>
2025-05-05 14:00:41 -0500

    estructura lista

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  output: "static",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
